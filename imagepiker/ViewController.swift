e9c1149d4c932f88d6224284036d25f7278db9b5
mohamed <mohamed@faress-mac-3.local>
2019-01-01 10:02:23 +0200

    Alert

--- imagepiker/ViewController.swift
+++ imagepiker/ViewController.swift
@@ -17,9 +17,15 @@ class ViewController: UIViewController {
 
 
     @IBAction func exprement() {
-        let image=UIImage()
-        let nextController=UIActivityViewController(activityItems: [image], applicationActivities: nil)
+  
+        let nextController=UIAlertController()
+        nextController.title="TestAlert"
+        nextController.message="this is alert"
         present(nextController,animated:true,completion: nil )
+        let OKAction=UIAlertAction(title: "ok", style: UIAlertAction.Style.default) { action in
+            self.dismiss(animated: true, completion: nil)
+        }
+         nextController.addAction(OKAction)
     }
     
 }
